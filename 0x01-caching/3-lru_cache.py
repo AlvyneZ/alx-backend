f0f3724018e267c842f427ad3f75d9d4d3c569fb
AlvyneZ <alvynew@gmail.com>
2024-01-03 20:08:57 +0300

    LRU cahce implementation

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """
-2-lifo_cache.py - Provides one class:
-    LIFOCache(BaseCaching)
+3-lru_cache.py - Provides one class:
+    LRUCache(BaseCaching)
 """
 
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """
-    LIFOCache implements a caching system of size MAX_ITEMS
-     that removes the newest inserted data
+    LRUCache implements a caching system of size MAX_ITEMS
+     that removes the least recently used (accessed/inserted)
+     data
     """
     def __init__(self):
         """Initialize the age of data
@@ -26,11 +27,10 @@ class LIFOCache(BaseCaching):
             return
         if self.cache_data.get(key, None) is None:
             if len(self.keys) >= self.MAX_ITEMS:
-                print("DISCARD: {}".format(self.keys[self.MAX_ITEMS - 1]))
-                self.cache_data.pop(self.keys[self.MAX_ITEMS - 1])
-                self.keys[self.MAX_ITEMS - 1] = key
-            else:
-                self.keys.append(key)
+                print("DISCARD: {}".format(self.keys[0]))
+                self.cache_data.pop(self.keys[0])
+                self.keys.pop(0)
+            self.keys.append(key)
         else:
             # Update edits to become the new last in
             self.keys.remove(key)
@@ -40,21 +40,38 @@ class LIFOCache(BaseCaching):
     def get(self, key):
         """ Get an item by key
         """
-        return self.cache_data.get(key, None)
+        data = self.cache_data.get(key, None)
+        if data is not None:
+            self.keys.remove(key)
+            self.keys.append(key)
+        return data
+
 
 
 if __name__ == "__main__":
-    my_cache = LIFOCache()
+    my_cache = LRUCache()
     my_cache.put("A", "Hello")
     my_cache.put("B", "World")
     my_cache.put("C", "Holberton")
     my_cache.put("D", "School")
     my_cache.print_cache()
+    print(my_cache.get("B"))
     my_cache.put("E", "Battery")
     my_cache.print_cache()
     my_cache.put("C", "Street")
     my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
     my_cache.put("F", "Mission")
     my_cache.print_cache()
     my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
     my_cache.print_cache()
