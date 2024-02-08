c8cea61735b08542bee9d96e45cd428dfea918c1
AlvyneZ <alvynew@gmail.com>
2024-02-08 23:22:31 +0300

    Removing unused variable

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'redis';
 
-const client = createClient()
+createClient()
   .on('error', (err) => {
     console.log(`Redis client not connected to the server: ${err}`);
   })
